1b77320cf33f493b1e60f1703d6757c035e9ced2
zsadov1 <zsadov1@gmail.com>
2019-11-21 15:59:45 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//generate random password
+function generate(){
+
+    //set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+    //possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    // create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+}
+}
